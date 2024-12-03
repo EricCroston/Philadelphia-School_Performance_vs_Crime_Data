eb71a889b74afd43a84fc314314be8b462dc30d0
EricCroston <ecroston@gmail.com>
2024-12-02 23:15:46 -0500

    database key constraints and data integrity

--- Building SQL Database/Schema-Philly-School_vs_Crime.sql
+++ Building SQL Database/Schema-Philly-School_vs_Crime.sql
@@ -4,7 +4,7 @@
 
 
 CREATE TABLE "School_Info" (
-    "school_id" int32   NOT NULL,
+    "school_id" int   NOT NULL,
     "school_name" varchar   NOT NULL,
     "school_name_label" varchar   NOT NULL,
     "street_address" varchar   NOT NULL,
@@ -12,9 +12,9 @@ CREATE TABLE "School_Info" (
     "phone_number" varchar   NOT NULL,
     "grade_level" varchar   NOT NULL,
     "grade_org" varchar   NOT NULL,
-    "type" int64   NOT NULL,
+    "type" int   NOT NULL,
     "type_specific" varchar   NOT NULL,
-    "geometry" geometry   NOT NULL,
+    "geometry" varchar   NOT NULL,
     "district" varchar   NOT NULL,
     "district_psa" varchar   NOT NULL,
     CONSTRAINT "pk_School_Info" PRIMARY KEY (
@@ -24,28 +24,27 @@ CREATE TABLE "School_Info" (
 
 CREATE TABLE "High_School_Metric_Scores" (
     "year" varchar   NOT NULL,
-    "school_id" int64   NOT NULL,
+    "school_id" int   NOT NULL,
     "school_name" varchar   NOT NULL,
     "report_type" varchar   NOT NULL,
     "admission_type" varchar   NOT NULL,
-    "number_students_served_full_year" int64   NOT NULL,
-    "october_one_enrollment" int64   NOT NULL,
-    "percent_11th_grade_students_proficient_all_three_keystones" float64   NOT NULL,
-    "percent_11th_grade_students_below_basic_keystone_algebra_i" float64   NOT NULL,
-    "percent_11th_grade_students_proficient_keystone_algebra_i" float64   NOT NULL,
-    "percent_11th_grade_students_below_basic_keystone_biology" float64   NOT NULL,
-    "percent_11th_grade_students_proficient_keystone_biology" float64   NOT NULL,
-    "percent_11th_grade_students_below_basic_keystone_literature" float64   NOT NULL,
-    "percent_11th_grade_students_proficient_keystone_literature" float64   NOT NULL,
-    "percent_12th_grade_cte_students_meeting_industry_standards" float64   NOT NULL,
-    "ninth_grade_on_track_rate" float64   NOT NULL,
-    "percent_metrics_improving_all_students" float64   NOT NULL,
-    "percent_students_attending_at_least_90_percent" float64   NOT NULL,
-    "number_dropouts_grades_7_to_12" int64   NOT NULL,
-    "percent_students_receiving_zero_out_of_school_suspensions" float64   NOT NULL,
-    "percent_students_completing_the_fafsa_by_end_of_12th_grade" float64   NOT NULL,
-    "first_fall_matriculation" float64   NOT NULL,
-    "four_year_cohort_graduation_rate" float64   NOT NULL,
+    "number_students_served_full_year" int   NOT NULL,
+    "october_one_enrollment" int   NOT NULL,
+    "percent_11th_grade_students_proficient_all_three_keystones" float   NOT NULL,
+    "percent_11th_grade_students_below_basic_keystone_algebra_i" float   NOT NULL,
+    "percent_11th_grade_students_proficient_keystone_algebra_i" float   NOT NULL,
+    "percent_11th_grade_students_below_basic_keystone_biology" float   NOT NULL,
+    "percent_11th_grade_students_proficient_keystone_biology" float   NOT NULL,
+    "percent_11th_grade_students_below_basic_keystone_literature" float   NOT NULL,
+    "percent_11th_grade_students_proficient_keystone_literature" float   NOT NULL,
+    "ninth_grade_on_track_rate" float   NOT NULL,
+    "percent_metrics_improving_all_students" float   NOT NULL,
+    "percent_students_attending_at_least_90_percent" float   NOT NULL,
+    "number_dropouts_grades_7_to_12" int   NOT NULL,
+    "percent_students_receiving_zero_out_of_school_suspensions" float   NOT NULL,
+    "percent_students_completing_the_fafsa_by_end_of_12th_grade" float   NOT NULL,
+    "first_fall_matriculation" float   NOT NULL,
+    "four_year_cohort_graduation_rate" float   NOT NULL,
     CONSTRAINT "pk_High_School_Metric_Scores" PRIMARY KEY (
         "school_name"
      )
@@ -53,22 +52,22 @@ CREATE TABLE "High_School_Metric_Scores" (
 
 CREATE TABLE "Middle_School_Metric_Scores" (
     "year" varchar   NOT NULL,
-    "school_id" int64   NOT NULL,
+    "school_id" int   NOT NULL,
     "school_name" varchar   NOT NULL,
     "report_type" varchar   NOT NULL,
     "admission_type" varchar   NOT NULL,
-    "number_students_served_full_year" int64   NOT NULL,
-    "october_one_enrollment" int64   NOT NULL,
-    "pssa_ela_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_below_basic_grades_4_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float64   NOT NULL,
-    "percent_metrics_improving_all_students" float64   NOT NULL,
-    "percent_students_attending_at_least_90_percent" float64   NOT NULL,
-    "number_dropouts_grades_7_to_12" int64   NOT NULL,
-    "percent_students_receiving_zero_out_of_school_suspensions" float64   NOT NULL,
+    "number_students_served_full_year" int   NOT NULL,
+    "october_one_enrollment" int   NOT NULL,
+    "pssa_ela_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_science_percent_students_below_basic_grades_4_to_8" float   NOT NULL,
+    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float   NOT NULL,
+    "percent_metrics_improving_all_students" float   NOT NULL,
+    "percent_students_attending_at_least_90_percent" float   NOT NULL,
+    "number_dropouts_grades_7_to_12" int   NOT NULL,
+    "percent_students_receiving_zero_out_of_school_suspensions" float   NOT NULL,
     CONSTRAINT "pk_Middle_School_Metric_Scores" PRIMARY KEY (
         "school_name"
      )
@@ -76,26 +75,26 @@ CREATE TABLE "Middle_School_Metric_Scores" (
 
 CREATE TABLE "KtoEight_School_Metric_Scores" (
     "year" varchar   NOT NULL,
-    "school_id" int64   NOT NULL,
+    "school_id" int   NOT NULL,
     "school_name" varchar   NOT NULL,
     "report_type" varchar   NOT NULL,
     "admission_type" varchar   NOT NULL,
-    "number_students_served_full_year" int64   NOT NULL,
-    "october_one_enrollment" int64   NOT NULL,
-    "pssa_ela_percent_students_below_basic_grade_3" float64   NOT NULL,
-    "pssa_ela_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_ela_percent_students_proficient_or_advanced_grade_3" float64   NOT NULL,
-    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_below_basic_grade_3" float64   NOT NULL,
-    "pssa_math_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_proficient_or_advanced_grade_3" float64   NOT NULL,
-    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_below_basic_grades_4_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float64   NOT NULL,
-    "percent_metrics_improving_all_students" float64   NOT NULL,
-    "percent_students_attending_at_least_90_percent" float64   NOT NULL,
-    "number_dropouts_grades_7_to_12" int64   NOT NULL,
-    "percent_students_receiving_zero_out_of_school_suspensions" float64   NOT NULL,
+    "number_students_served_full_year" int   NOT NULL,
+    "october_one_enrollment" int   NOT NULL,
+    "pssa_ela_percent_students_below_basic_grade_3" float   NOT NULL,
+    "pssa_ela_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_ela_percent_students_proficient_or_advanced_grade_3" float   NOT NULL,
+    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_below_basic_grade_3" float   NOT NULL,
+    "pssa_math_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_proficient_or_advanced_grade_3" float   NOT NULL,
+    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_science_percent_students_below_basic_grades_4_to_8" float   NOT NULL,
+    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float   NOT NULL,
+    "percent_metrics_improving_all_students" float   NOT NULL,
+    "percent_students_attending_at_least_90_percent" float   NOT NULL,
+    "number_dropouts_grades_7_to_12" int   NOT NULL,
+    "percent_students_receiving_zero_out_of_school_suspensions" float   NOT NULL,
     CONSTRAINT "pk_KtoEight_School_Metric_Scores" PRIMARY KEY (
         "school_name"
      )
@@ -103,25 +102,25 @@ CREATE TABLE "KtoEight_School_Metric_Scores" (
 
 CREATE TABLE "Elementary_School_Metric_Scores" (
     "year" varchar   NOT NULL,
-    "school_id" int64   NOT NULL,
+    "school_id" int   NOT NULL,
     "school_name" varchar   NOT NULL,
     "report_type" varchar   NOT NULL,
     "admission_type" varchar   NOT NULL,
-    "number_students_served_full_year" int64   NOT NULL,
-    "october_one_enrollment" int64   NOT NULL,
-    "pssa_ela_percent_students_below_basic_grade_3" float64   NOT NULL,
-    "pssa_ela_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_ela_percent_students_proficient_or_advanced_grade_3" float64   NOT NULL,
-    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_below_basic_grade_3" float64   NOT NULL,
-    "pssa_math_percent_students_below_basic_grades_3_to_8" float64   NOT NULL,
-    "pssa_math_percent_students_proficient_or_advanced_grade_3" float64   NOT NULL,
-    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_below_basic_grades_4_to_8" float64   NOT NULL,
-    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float64   NOT NULL,
-    "percent_metrics_improving_all_students" float64   NOT NULL,
-    "percent_students_attending_at_least_90_percent" float64   NOT NULL,
-    "percent_students_receiving_zero_out_of_school_suspensions" float64   NOT NULL,
+    "number_students_served_full_year" int   NOT NULL,
+    "october_one_enrollment" int   NOT NULL,
+    "pssa_ela_percent_students_below_basic_grade_3" float   NOT NULL,
+    "pssa_ela_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_ela_percent_students_proficient_or_advanced_grade_3" float   NOT NULL,
+    "pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_below_basic_grade_3" float   NOT NULL,
+    "pssa_math_percent_students_below_basic_grades_3_to_8" float   NOT NULL,
+    "pssa_math_percent_students_proficient_or_advanced_grade_3" float   NOT NULL,
+    "pssa_math_percent_students_proficient_or_advanced_grades_3_to_8" float   NOT NULL,
+    "pssa_science_percent_students_below_basic_grades_4_to_8" float   NOT NULL,
+    "pssa_science_percent_students_proficient_or_advanced_grades_4_to_8" float   NOT NULL,
+    "percent_metrics_improving_all_students" float   NOT NULL,
+    "percent_students_attending_at_least_90_percent" float   NOT NULL,
+    "percent_students_receiving_zero_out_of_school_suspensions" float   NOT NULL,
     CONSTRAINT "pk_Elementary_School_Metric_Scores" PRIMARY KEY (
         "school_name"
      )
@@ -129,39 +128,39 @@ CREATE TABLE "Elementary_School_Metric_Scores" (
 
 CREATE TABLE "Police_Districts" (
     "district_psa" varchar   NOT NULL,
-    "district" int64   NOT NULL,
+    "district" int   NOT NULL,
     "police_service_area" varchar   NOT NULL,
     "location" varchar   NOT NULL,
-    "zip_code" int64   NOT NULL,
+    "zip_code" int   NOT NULL,
     "phone_number" varchar   NOT NULL,
-    "geometry" geometry   NOT NULL,
+    "geometry" varchar   NOT NULL,
     CONSTRAINT "pk_Police_Districts" PRIMARY KEY (
         "district_psa"
      )
 );
 
 CREATE TABLE "Police_Location" (
-    "district" int64   NOT NULL,
+    "district" int   NOT NULL,
     "location" varchar   NOT NULL,
-    "zip_code" int64   NOT NULL,
+    "zip_code" int   NOT NULL,
     "phone_number" varchar   NOT NULL,
-    "geometry" geometry   NOT NULL,
+    "geometry" varchar   NOT NULL,
     CONSTRAINT "pk_Police_Location" PRIMARY KEY (
         "district"
      )
 );
 
 CREATE TABLE "Incidents_Time_Place" (
-    "incident_id" int64   NOT NULL,
-    "district" int64   NOT NULL,
+    "incident_id" int   NOT NULL,
+    "district" int   NOT NULL,
     "police_service_area" varchar   NOT NULL,
     "district_psa" varchar   NOT NULL,
     "dispatch_date_and_time" timestamp   NOT NULL,
     "dispatch_date" date   NOT NULL,
     "dispatch_time" varchar   NOT NULL,
-    "dispatch_hour" float64   NOT NULL,
+    "dispatch_hour" float   NOT NULL,
     "location_block" varchar   NOT NULL,
-    "ucr_code" int64   NOT NULL,
+    "ucr_code" int   NOT NULL,
     "general_crime_category" varchar   NOT NULL,
     "crime_group" varchar   NOT NULL,
     CONSTRAINT "pk_Incidents_Time_Place" PRIMARY KEY (
@@ -170,9 +169,9 @@ CREATE TABLE "Incidents_Time_Place" (
 );
 
 CREATE TABLE "Incidents" (
-    "ucr_code" int64   NOT NULL,
-    "general_crime_category" object   NOT NULL,
-    "crime_group" object   NOT NULL,
+    "ucr_code" int   NOT NULL,
+    "general_crime_category" varchar   NOT NULL,
+    "crime_group" varchar   NOT NULL,
     CONSTRAINT "pk_Incidents" PRIMARY KEY (
         "ucr_code"
      )
