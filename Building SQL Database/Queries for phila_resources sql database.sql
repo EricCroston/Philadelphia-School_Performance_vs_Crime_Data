719da860015ffd07a2b7178acb145c27acc70ea8
EricCroston <ecroston@gmail.com>
2024-12-03 15:32:32 -0500

    database outputs

--- Building SQL Database/Queries for phila_resources sql database.sql
+++ Building SQL Database/Queries for phila_resources sql database.sql
@@ -1,258 +1,292 @@
-SELECT * FROM elementary_school_metric_scores
-
-SELECT * FROM high_school_metric_scores
-
-SELECT * FROM ktoeight_school_metric_scores
-
-SELECT * FROM middle_school_metric_scores
-
-SELECT * FROM "School_List"
-
-SELECT * FROM new_geo_schools_df
-
-SELECT * FROM "PoliceLocation"
-
-SELECT * FROM "IncidentsTimePlace" 
-
+------Verify the Tables---------------------------------
+SELECT * FROM "Elementary_School_Metric_Scores"
+SELECT * FROM "KtoEight_School_Metric_Scores"
+SELECT * FROM "Middle_School_Metric_Scores"
+SELECT * FROM "High_School_Metric_Scores"
+SELECT * FROM "School_Info"
+SELECT * FROM "Police_Location"
+SELECT * FROM "Police_Districts"
 SELECT * FROM "Incidents"
+SELECT * FROM "Incidents_Time_Place"
 
-SELECT * FROM "Districts"
 
 ------Police Data Analysis---------------------------------
 
----1. List the District_PSA, , first name, sex, and salary of each employee.
-SELECT i."District_PSA", i."UCR_Code", i."General_Crime_Category", i."Crime_Group"
-FROM "IncidentsTimePlace" i
+---1. List the district_psa, , ucr_code, general_crime_category and crime_group for each incident
+SELECT i."district_psa", i."ucr_code", i."general_crime_category", i."crime_group"
+FROM "Incidents_Time_Place" i;
 
 --2. List the frequency counts, in descending order, of General_Crime_Category (that is, how many of each crime).
-SELECT "General_Crime_Category", COUNT("General_Crime_Category") AS "Frequency"
-FROM "IncidentsTimePlace"
-GROUP BY "General_Crime_Category"
-ORDER BY COUNT("General_Crime_Category") DESC;
+SELECT "general_crime_category", COUNT("general_crime_category") AS "frequency"
+FROM "Incidents_Time_Place"
+GROUP BY "general_crime_category"
+ORDER BY COUNT("general_crime_category") DESC;
 
 --3. List the frequency counts, in descending order, of General_Crime_Category and Date (that is, how many of each crime).
 SELECT 
-    "Dispatch_Date",
-    "General_Crime_Category", 
-    COUNT("General_Crime_Category") AS "Frequency"
+    "dispatch_date", 
+    "general_crime_category", 
+    COUNT("general_crime_category") AS "frequency"
 FROM 
-    "IncidentsTimePlace"
+    "Incidents_Time_Place"
 GROUP BY 
-    "Dispatch_Date", "General_Crime_Category"
+    "dispatch_date", 
+    "general_crime_category"
 ORDER BY 
-    "Dispatch_Date", COUNT("General_Crime_Category") DESC;
+    "dispatch_date", 
+    COUNT("general_crime_category") DESC;
 
 --4.  List the frequency counts, in descending order, of Crime_Group and Date (that is, how many of each crime group).
 SELECT 
-    "Dispatch_Date",
-    "Crime_Group", 
-    COUNT("Crime_Group") AS "Frequency"
+    "dispatch_date", 
+    "crime_group", 
+    COUNT("crime_group") AS "frequency"
 FROM 
-    "IncidentsTimePlace"
+    "Incidents_Time_Place"
 GROUP BY 
-    "Dispatch_Date", "Crime_Group"
+    "dispatch_date", 
+    "crime_group"
 ORDER BY 
-    "Dispatch_Date", COUNT("Crime_Group") DESC;
+    "dispatch_date", 
+    COUNT("crime_group") DESC;
 
 
 --5. List the frequency of each type of crime within each District_PSA, group by both District_PSA and General_Crime_Category. 
+SELECT 
+    "district_psa", 
+    "general_crime_category", 
+    COUNT(*) AS "frequency"
+FROM 
+    "Incidents_Time_Place"
+GROUP BY 
+    "district_psa", 
+    "general_crime_category"
+ORDER BY 
+    "district_psa", 
+    COUNT(*) DESC;
 
-SELECT "District_PSA", "General_Crime_Category", COUNT(*) AS "Frequency"
-FROM "IncidentsTimePlace"
-GROUP BY "District_PSA", "General_Crime_Category"
-ORDER BY "District_PSA", COUNT(*) DESC;
-
---6. List the totals each type of crime within each District_PSA,
-
-SELECT "District_PSA", COUNT(*) AS "Total_Crimes"
-FROM "IncidentsTimePlace"
-GROUP BY "District_PSA"
-ORDER BY COUNT(*) DESC;
+--6. List the totals for each type of crime within each District_PSA,
+SELECT 
+    "district_psa", 
+    COUNT(*) AS "total_crimes"
+FROM 
+    "Incidents_Time_Place"
+GROUP BY 
+    "district_psa"
+ORDER BY 
+    "district_psa", 
+    COUNT(*) DESC;
 
 --7. List the totals each type of crime within each District_PSA,
-
-SELECT "District_PSA", COUNT(*) AS "Total_Crimes"
-FROM "IncidentsTimePlace"
-GROUP BY "District_PSA"
-ORDER BY COUNT(*) DESC;
+SELECT 
+    "district_psa", 
+    COUNT(*) AS "total_Crimes"
+FROM 
+    "Incidents_Time_Place"
+GROUP BY 
+    "district_psa"
+ORDER BY 
+    "district_psa", 
+    COUNT(*) DESC;
 
 ---8. List the District_PSA, Crime Group and Frequncy.
-SELECT "District_PSA", "Crime_Group", COUNT(*) AS "Frequency"
-FROM "IncidentsTimePlace"
-GROUP BY "District_PSA", "Crime_Group"
-ORDER BY "District_PSA", COUNT(*) DESC;
+SELECT 
+    "dispatch_date", 
+    "crime_group", 
+    COUNT(*) AS "frequency"
+FROM 
+    "Incidents_Time_Place"
+GROUP BY 
+    "dispatch_date", 
+    "crime_group"
+ORDER BY 
+    "dispatch_date", 
+    COUNT(*) DESC;
+
 
 -----------High School Metrics Analysis------------ 
 
 ---9. List Highschool Metrics with PSA and Crime Data
 SELECT 
-    ng."school_id",
-    ng."SCHOOL_NAME",
-    ng."District_PSA",
-    hs."Percent_Students_Attending_at_Least_90_Percent",
-    hs."Percent_11th_Grade_Students_Proficient_All_Three_Keystones"
+    si."school_id", 
+    si."school_name", 
+    si."district_psa", 
+    hs."percent_students_attending_at_least_90_percent", 
+    hs."percent_11th_grade_students_proficient_all_three_keystones"
 FROM 
-    "new_geo_schools_df" ng
-JOIN 
-    "high_school_metric_scores" hs
-ON 
-    ng."school_id" = hs."school_id"
+    "School_Info" si
+JOIN
+    "High_School_Metric_Scores" hs
+ON
+    si."school_id" = hs."school_id"
 ORDER BY 
-    ng."District_PSA";
+    si."district_psa";
 
 --10. Calculate the Average School Metrics for High Schools within the same PSA
 SELECT 
-    ng."District_PSA",
-    AVG(hs."Percent_Students_Attending_at_Least_90_Percent") AS "Average_Percent_Attending_90",
-    AVG(hs."Percent_11th_Grade_Students_Proficient_All_Three_Keystones") AS "Average_Percent_Proficient_All_Keystones"
+    si."district_psa", 
+    AVG(hs."percent_students_attending_at_least_90_percent") AS "average_percent_attending_90", 
+    AVG(hs."percent_11th_grade_students_proficient_all_three_keystones") AS "average_percent_proficient_all_keystones"
 FROM 
-    "new_geo_schools_df" ng
-JOIN 
-    "high_school_metric_scores" hs
-ON 
-    ng."school_id" = hs."school_id"
-GROUP BY 
-    ng."District_PSA"
+    "School_Info" si
+JOIN
+    "High_School_Metric_Scores" hs
+ON
+    si."school_id" = hs."school_id"
+GROUP BY
+    si."district_psa"
 ORDER BY 
-    ng."District_PSA";
+    si."district_psa";
 
 --11. Now include the total crimes for each crime group per PSA
-SELECT 
-    itp."District_PSA",
-    itp."Crime_Group",
-    COUNT(*) AS "Total_Crimes"
-FROM 
-    "IncidentsTimePlace" itp
-GROUP BY 
-    itp."District_PSA", itp."Crime_Group"
-ORDER BY 
-    itp."District_PSA", COUNT(*) DESC;
+SELECT
+    itp."district_psa", 
+    itp."crime_group", 
+    COUNT(*) AS "total_crimes"
+FROM
+    "Incidents_Time_Place" itp
+GROUP BY
+    itp."district_psa", 
+    itp."crime_group"
+ORDER BY
+    itp."district_psa", 
+    COUNT(*) DESC;
 
---12. Combined query from #7 and #8 queries
-
+--12. Combined query from #10 and #11 queries
 SELECT 
-    a."District_PSA",
-    a."Average_Percent_Attending_90",
-    a."Average_Percent_Proficient_All_Keystones",
-    b."Crime_Group",
-    b."Total_Crimes"
+    a."district_psa",
+    a."average_percent_attending_90",
+    a."average_percent_proficient_all_keystones",
+    b."crime_group",
+    b."total_crimes"
 FROM 
     (
         SELECT 
-            ng."District_PSA",
-            AVG(hs."Percent_Students_Attending_at_Least_90_Percent") AS "Average_Percent_Attending_90",
-            AVG(hs."Percent_11th_Grade_Students_Proficient_All_Three_Keystones") AS "Average_Percent_Proficient_All_Keystones"
-        FROM 
-            "new_geo_schools_df" ng
-        JOIN 
-            "high_school_metric_scores" hs ON ng."school_id" = hs."school_id"
-       
-        GROUP BY 
-            ng."District_PSA"
+                        si."district_psa", 
+                    AVG(hs."percent_students_attending_at_least_90_percent") AS "average_percent_attending_90", 
+                  AVG(hs."percent_11th_grade_students_proficient_all_three_keystones") AS "average_percent_proficient_all_keystones"
+                FROM 
+                    "School_Info" si
+                JOIN
+                    "High_School_Metric_Scores" hs
+                ON
+                    si."school_id" = hs."school_id"
+                GROUP BY
+                    si."district_psa"
     ) a
 JOIN 
     (
         SELECT 
-            itp."District_PSA",
-            itp."Crime_Group",
-            COUNT(*) AS "Total_Crimes"
+            itp."district_psa",
+            itp."crime_group",
+            COUNT(*) AS "total_crimes"
         FROM 
-            "IncidentsTimePlace" itp
+            "Incidents_Time_Place" itp
         GROUP BY 
-            itp."District_PSA", itp."Crime_Group"
-    ) b ON a."District_PSA" = b."District_PSA"
+            itp."district_psa", 
+                        itp."crime_group"
+    ) b 
+ON 
+    a."district_psa" = b."district_psa"
 ORDER BY 
-    a."District_PSA", b."Total_Crimes" DESC;
+    a."district_psa", 
+    b."district_psa" DESC;
 
 --13. Total Crimes and Average of High School Metrics
-
 SELECT 
-    a."District_PSA",
-    a."Average_Percent_Attending_90",
-    a."Average_Percent_Proficient_All_Keystones",
-    b."Total_Crimes"
+    a."district_psa",
+    a."average_percent_attending_90",
+    a."average_percent_proficient_all_keystones",
+    b."total_crimes"
 FROM 
     (
         SELECT 
-            ng."District_PSA",
-            AVG(hs."Percent_Students_Attending_at_Least_90_Percent") AS "Average_Percent_Attending_90",
-            AVG(hs."Percent_11th_Grade_Students_Proficient_All_Three_Keystones") AS "Average_Percent_Proficient_All_Keystones"
-        FROM 
-            "new_geo_schools_df" ng
-        JOIN 
-            "high_school_metric_scores" hs ON ng."school_id" = hs."school_id"
-       
-        GROUP BY 
-            ng."District_PSA"
+                        si."district_psa", 
+                    AVG(hs."percent_students_attending_at_least_90_percent") AS "average_percent_attending_90", 
+                  AVG(hs."percent_11th_grade_students_proficient_all_three_keystones") AS "average_percent_proficient_all_keystones"
+                FROM 
+                    "School_Info" si
+                JOIN
+                    "High_School_Metric_Scores" hs
+                ON
+                    si."school_id" = hs."school_id"
+                GROUP BY
+                    si."district_psa"
     ) a
 JOIN 
     (
         SELECT 
-            itp."District_PSA",
-            COUNT(*) AS "Total_Crimes"
+            itp."district_psa",
+            COUNT(*) AS "total_crimes"
         FROM 
-            "IncidentsTimePlace" itp
+            "Incidents_Time_Place" itp
         GROUP BY 
-            itp."District_PSA"
-    ) b ON a."District_PSA" = b."District_PSA"
+            itp."district_psa" 
+    ) b 
+ON 
+    a."district_psa" = b."district_psa"
 ORDER BY 
-    a."District_PSA", b."Total_Crimes" DESC;
+    a."district_psa";
 
 ---------k-8 School metrics compared to crime in same PSA---------------
 --14. Total Crimes and Average of k-8 School metrics 
-
 SELECT 
-    ng."District_PSA",
-    AVG(ks."PSSA_ELA_Percent_Students_Proficient_or_Advanced_Grades_3_to_8") AS "Average_ELA_Proficiency",
-    AVG(ks."PSSA_Math_Percent_Students_Proficient_or_Advanced_Grades_3_to_8") AS "Average_Math_Proficiency",
-	AVG(ks."Percent_Students_Attending_at_Least_90_Percent") AS "Average_Percent_Attending_90",
-    b."Total_Crimes"
+        si."district_psa", 
+        AVG(ks."pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8") AS "average_ela_proficiency", 
+    AVG(ks."pssa_math_percent_students_proficient_or_advanced_grades_3_to_8") AS "average_math_proficiency", 
+        AVG(ks."percent_students_attending_at_least_90_percent") AS "average_percent_attending_90"
 FROM 
-    "new_geo_schools_df" ng
-JOIN 
-    "ktoeight_school_metric_scores" ks ON ng."school_id" = ks."school_id"
+        "School_Info" si
+JOIN
+    "KtoEight_School_Metric_Scores" ks
+ON
+    si."school_id" = ks."school_id"
 JOIN 
     (
         SELECT 
-            itp."District_PSA",
-            COUNT(*) AS "Total_Crimes"
+            itp."district_psa",
+            COUNT(*) AS "total_crimes"
         FROM 
-            "IncidentsTimePlace" itp
+            "Incidents_Time_Place" itp
         GROUP BY 
-            itp."District_PSA"
-    ) b ON ng."District_PSA" = b."District_PSA"
-
-GROUP BY 
-    ng."District_PSA", b."Total_Crimes"
+            itp."district_psa" 
+    ) b 
+ON 
+        si."district_psa" = b."district_psa"
+GROUP BY
+    si."district_psa", 
+        b."total_crimes"
 ORDER BY 
-    ng."District_PSA";
+    si."district_psa";
 
 -----elementary metrics and crime data for same PSA--------------
 
 ---15. Total Crimes and Average of elementary School metrics 
-
 SELECT 
-    ng."District_PSA",
-    AVG(es."PSSA_ELA_Percent_Students_Proficient_or_Advanced_Grades_3_to_8") AS "Average_ELA_Proficiency_3_to_8",
-    AVG(es."PSSA_Math_Percent_Students_Proficient_or_Advanced_Grades_3_to_8") AS "Average_Math_Proficiency_3_to_8",
-    AVG(es."Percent_Students_Attending_at_Least_90_Percent") AS "Average_Percent_Attending_90",
-	b."Total_Crimes"
+        si."district_psa", 
+        AVG(es."pssa_ela_percent_students_proficient_or_advanced_grades_3_to_8") AS "average_ela_proficiency", 
+        AVG(es."pssa_math_percent_students_proficient_or_advanced_grades_3_to_8") AS "average_math_proficiency", 
+        AVG(es."percent_students_attending_at_least_90_percent") AS "average_percent_attending_90"
 FROM 
-    "new_geo_schools_df" ng
-JOIN 
-    "elementary_school_metric_scores" es ON ng."school_id" = es."school_id"
+        "School_Info" si
+JOIN
+    "Elementary_School_Metric_Scores" es
+ON
+    si."school_id" = es."school_id"
 JOIN 
     (
         SELECT 
-            itp."District_PSA",
-            COUNT(*) AS "Total_Crimes"
+            itp."district_psa",
+            COUNT(*) AS "total_crimes"
         FROM 
-            "IncidentsTimePlace" itp
+            "Incidents_Time_Place" itp
         GROUP BY 
-            itp."District_PSA"
-    ) b ON ng."District_PSA" = b."District_PSA"
-
-GROUP BY 
-    ng."District_PSA", b."Total_Crimes"
+            itp."district_psa" 
+    ) b 
+ON 
+        si."district_psa" = b."district_psa"
+GROUP BY
+    si."district_psa", 
+        b."total_crimes"
 ORDER BY 
-    ng."District_PSA";
+    si."district_psa";
